39892ce41e18649c4528ee613a9c6e2e2db4e9d2
Shaun Mendes <shaunkmmendes@gmail.com>
2024-11-10 14:59:08 +0530

    marketplace

---
+++ Website/ecosphere-final/src/app/marketplace/cart/page.tsx
@@ -0,0 +1,9 @@
+"use server"
+import { Cart } from "../components/market_component/cart";
+export default async function CartPage() {
+  return (
+    <div>
+      <Cart />
+    </div>
+  );
+}
---
+++ Website/ecosphere-final/src/app/marketplace/page.tsx
@@ -0,0 +1,9 @@
+import { Marketplacetest } from "../../app/marketplace/components/market_component/Marketplacetest";
+
+export default function MarketplacePage() {
+  return (
+    <div>
+      <Marketplacetest />
+    </div>
+  );
+}
